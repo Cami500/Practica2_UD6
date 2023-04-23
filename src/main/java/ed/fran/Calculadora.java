810a2985db34c5bebb40111d1e3f9da31ac118e9
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-04-23 16:30:09 +0200

    Expicación de cada método

--- src/main/java/ed/fran/Calculadora.java
+++ src/main/java/ed/fran/Calculadora.java
@@ -13,19 +13,57 @@ public class Calculadora {
 	@version 3.7
 	@since 2.1 junio/2022
 	 */
-	
+
+	/*
+	@description devolver la suma de dos números enteros pasados como parámetro
+	@param int a
+	@param int b
+	@return devuele la suma de dos números enteros
+	@throws error al introducir un tipo de dato no esperado, es decir,
+	un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	 */
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+
+	/*
+	@description devolver la resta de dos números enteros pasados como parámetro
+	@param int a
+	@param int b
+	@return devuele la resta de dos números enteros
+	@throws error al introducir un tipo de dato no esperado, es decir,
+	un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	 */
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	
+
+	/*
+	@description devolver la multpilicación de dos números enteros pasados como parámetro
+	@param int a
+	@param int b
+	@return devuele la multiplicación de dos números enteros
+	@throws error al introducir un tipo de dato no esperado, es decir,
+	un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	 */
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
+
+	/*
+	@description devolver la división de dos números enteros pasados como parámetro
+	@param int a
+	@param int b
+	@return devuele la división de dos números enteros
+	@throws error al introducir un tipo de dato no esperado, es decir,
+	un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	Además de una posible opción, dividir un número entero,
+	esto genera una excepción del tipo ArithmeticException, no es de obligada captura.
+	No son es obligada captura, por lo tanto son de tipo RunTimeException
+	 */
 	public int diveix(int a, int b) {
 		return a/b;
 	}

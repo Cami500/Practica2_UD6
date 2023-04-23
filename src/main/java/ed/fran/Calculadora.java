2917847da5a4100890127e697bced46bb977576c
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-04-23 17:57:38 +0200

    Elimnado el @description no nos sirve

--- src/main/java/ed/fran/Calculadora.java
+++ src/main/java/ed/fran/Calculadora.java
@@ -1,68 +1,68 @@
 package ed.fran;
 
-public class Calculadora {
-	// Calculadora que nos permte, sumar,restar, multiplicar, dividir.
-	/*
-	Applicación que nos permite realizar varias operaciones enteras;
-	sumar
-	restar
-	multiplicar
-	dividir
-	Todas estas operaciones, son llevadas a cabo mediante dos números enteros
-	@author Fran Gregori
-	@version 3.7
-	@since 2.1 junio/2022
+// Calculadora que nos permte, sumar,restar, multiplicar, dividir.
+	/**
+	 * Applicación que nos permite realizar varias operaciones enteras;
+	 * sumar
+	 * restar
+	 * multiplicar
+	 * dividir
+	 * Todas estas operaciones, son llevadas a cabo mediante dos números enteros
+	 * @author Fran Gregori
+	 * @version 3.7
+	 * @since 2.1 junio/2022
 	 */
+public class Calculadora {
 
-	/*
-	@description devolver la suma de dos números enteros pasados como parámetro
-	@param int a
-	@param int b
-	@return devuele la suma de dos números enteros
-	@throws error al introducir un tipo de dato no esperado, es decir,
-	un tipo de dato superior al int o diferente al int, tales como String, char etc...
-	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	/**
+	 * Devolver la suma de dos números enteros pasados como parámetro
+	 * @param a de tipo entero
+	 * @param b de tipo entero
+	 * @return devuele la suma de dos números enteros
+	 * @throws RuntimeException Tipo de dato no esperado, es decir,
+	 * un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	 * No son de obligada captura
 	 */
 	public int suma(int a, int b) {
 		return a + b;
 	}
 
-	/*
-	@description devolver la resta de dos números enteros pasados como parámetro
-	@param int a
-	@param int b
-	@return devuele la resta de dos números enteros
-	@throws error al introducir un tipo de dato no esperado, es decir,
-	un tipo de dato superior al int o diferente al int, tales como String, char etc...
-	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	/**
+	* Devolver la resta de dos números enteros pasados como parámetro
+	* @param a de tipo entero
+	* @param b de tipo entero
+	* @return devuele la resta de dos números enteros
+	* @throws RuntimeException Tipo de dato no esperado, es decir,
+	* un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	* No son de obligada captura, por lo tanto son de tipo RunTimeException
 	 */
 	public int resta(int a, int b) {
 		return a - b;
 	}
 
-	/*
-	@description devolver la multpilicación de dos números enteros pasados como parámetro
-	@param int a
-	@param int b
-	@return devuele la multiplicación de dos números enteros
-	@throws error al introducir un tipo de dato no esperado, es decir,
-	un tipo de dato superior al int o diferente al int, tales como String, char etc...
-	No son de obligada captura, por lo tanto son de tipo RunTimeException
+	/**
+	 * Devolver la mutpilicación de dos números enteros pasados como parámetro
+	 * @param a de tipo entero
+	 * @param b de tipo entero
+	 * @return devuele la multpiplicacion de dos números enteros
+	 * @throws RuntimeException Tipo de dato no esperado, es decir,
+	 * un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	 * O dividir entre 0, no son de obligada captura
 	 */
 	public int multiplica(int a, int b){
 		return a*b;
 	}
 
-	/*
-	@description devolver la división de dos números enteros pasados como parámetro
-	@param int a
-	@param int b
-	@return devuele la división de dos números enteros
-	@throws error al introducir un tipo de dato no esperado, es decir,
-	un tipo de dato superior al int o diferente al int, tales como String, char etc...
-	Además de una posible opción, dividir un número entero,
-	esto genera una excepción del tipo ArithmeticException, no es de obligada captura.
-	No son es obligada captura, por lo tanto son de tipo RunTimeException
+	/**
+	* Devolver la división de dos números enteros pasados como parámetro
+	* @param a de tipo entero
+	* @param b de tipo entero
+	* @return devuele la división de dos números enteros
+	* @throws RuntimeException error al introducir un tipo de dato no esperado, es decir,
+	* un tipo de dato superior al int o diferente al int, tales como String, char etc...
+	* Además de una posible opción, dividir un número entero,
+	* esto genera una excepción del tipo ArithmeticException, no es de obligada captura.
+	* No son es obligada captura, por lo tanto son de tipo RunTimeException
 	 */
 	public int diveix(int a, int b) {
 		return a/b;

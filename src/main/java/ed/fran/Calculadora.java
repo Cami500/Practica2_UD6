006c6a34236ffdd6b5d0f0c55858f4412a6d9c26
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-04-23 16:14:08 +0200

    Parte 2.1 realizada, descripción corta y larga

--- src/main/java/ed/fran/Calculadora.java
+++ src/main/java/ed/fran/Calculadora.java
@@ -1,6 +1,18 @@
 package ed.fran;
 
 public class Calculadora {
+	// Calculadora que nos permte, sumar,restar, multiplicar, dividir.
+	/*
+	Applicación que nos permite realizar varias operaciones enteras;
+	sumar
+	restar
+	multiplicar
+	dividir
+	Todas estas operaciones, son llevadas a cabo mediante dos números enteros
+	@author Fran Gregori
+	@version 3.7
+	@since 2.1 junio/2022
+	 */
 	
 	public int suma(int a, int b) {
 		return a + b;

02749a6fb0ea3adcc7d60897a7ac0e0693d41175
IzanBlanes <izanblanesaracil@gmail.com>
2023-04-29 11:19:19 +0200

    Añadido el comentario normal con el parrafo con el desarrollo adecuado qué funciones realiza la clase Calculadora de la línea 6 al 10, y el comentario externo (descripción) añadico el autor del desarrollador, la versión del código y el depecadido que significa cuando se ha creado la clase años atrás de esta clase de la línea 12 al 17.

--- src/main/java/ed/izan/Calculadora.java
+++ src/main/java/ed/izan/Calculadora.java
@@ -1,7 +1,20 @@
 package ed.izan;
 
 public class Calculadora {
-	//Esta clase permite realizar cálculos matemáticos entre dos números enteros
+	//Esta clase permite realizar cálculos matemáticos entre dos números enteros.
+
+	/*
+	* La clase Calculadora permite realizar cálculos matemáticos entre dos números enteros. Además la clase contiene los
+	* métodos de sumar, restar, multiplicar y dividir para que después, se llamen creando el objeto calculadora
+	* dependiendo del constructor que se crea, porque estos métodos son objetos, es decir de la propia clase.
+	*/
+
+	/**
+	 *
+	 * @author Izan Blanes Aracil
+	 * @version 3.7
+	 * @deprecated 2.1 junio de 2022;
+	 **/
 	public int suma(int a, int b) {
 		return a + b;
 	}

c290c623ac7bd01062a605fdf55bb5f7eb900391
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-05-02 17:08:27 +0200

    [MOD] Sexta Tarea

--- src/main/java/ed/andreu/Calculadora.java
+++ src/main/java/ed/andreu/Calculadora.java
@@ -1,12 +1,12 @@
 package ed.andreu;
-	/**
-	 * Esta clase sirve para una calculadora como en sus metodos uno sirva para somar dos numeros, otro metodo
-	 sirve para restar dos numeros, otro para multiplicar dos numeros y otro para dividir dos numeros.
+/**
+ * Esta clase sirve para una calculadora como en sus metodos uno sirva para somar dos numeros, otro metodo
+ sirve para restar dos numeros, otro para multiplicar dos numeros y otro para dividir dos numeros.
 
-	 @author  Andreu Puchades Pascual
-	 @version La versión actual de la clase (3.7)
-	 @since Está disponible desde la versión 2.1, junio de 2022.
-	 **/
+ @author  Andreu Puchades Pascual
+ @version La versión actual de la clase (3.7)
+ @since Está disponible desde la versión 2.1, junio de 2022.
+ **/
 
 public class Calculadora {
 	/**
@@ -56,5 +56,60 @@ public class Calculadora {
 		}
 
 		return a / b;
+	}
+
+	/**
+	 * Calcula la potencia de un número elevado a un exponente dado.
+	 *
+	 * @param base la base de la potencia.
+	 * @param exponente el exponente de la potencia.
+	 * @return el resultado de la potencia.
+	 */
+	public int calcularPotencia(int base, int exponente) {
+
+		int resultado = 1;
+
+		for (int i = 0; i < exponente; i++) {
+			resultado *= base;
+		}
+
+		return resultado;
+	}
+
+	/**
+	 * Determina si un número es par o impar.
+	 *
+	 * @param numero el número a evaluar.
+	 * @return true si el número es par, false si es impar.
+	 */
+	public boolean esNumeroPar(int numero) {
+
+		boolean resultado;
+
+		if (numero % 2 == 0) {
+			resultado = true;
+		} else {
+			resultado = false;
+		}
+
+		return resultado;
+	}
+
+	/**
+	 * Convierte un número entero en base decimal a su equivalente en base binaria.
+	 * @param decimal el número en base decimal a convertir.
+	 * @return el número en base binaria como una cadena de caracteres.
+	 */
+	public String convertirDecimalABinario(int decimal) {
+		String binario = "";
+		int nuevo;
+
+		while (decimal > 0) {
+			nuevo = decimal % 2;
+			binario = nuevo + binario;
+			decimal = decimal / 2;
+		}
+
+		return binario;
 	}
 }

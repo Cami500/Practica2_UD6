bdb695d50627ce58fb93ea96d2a86d68591cfb66
TATIANA <tatianaypunto@gmail.com>
2023-05-03 14:35:02 +0200

    métodos documentados

--- src/main/java/ed/tatiana/Calculadora.java
+++ src/main/java/ed/tatiana/Calculadora.java
@@ -19,15 +19,32 @@ public class Calculadora {
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+
+	/** Realiza la operación de resta entre dos números enteros.
+	 * @param a el primer número entero
+	 * @param b el segundo número entero
+	 * @return el resultado de la resta de a y b
+	 */
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	
+
+	/**Realiza la multiplicación entre dos números enteros.
+ 	 * @param a el primer número entero
+	 * @param b el segundo número entero
+	 * @return el resultado de la multiplicación de a y b
+	 */
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
+
+	/** Realiza la operación de división entre dos números enteros.
+	 * Si el segundo número es cero, se lanza una excepción ArithmeticException.
+	 * @param a el primer número entero
+	 * @param b el segundo número entero
+	 * @return el resultado de la división de a entre b
+	 * @throws ArithmeticException si el segundo número (b) es cero.
+	 */
 	public int diveix(int a, int b) {
 		return a/b;
 	}

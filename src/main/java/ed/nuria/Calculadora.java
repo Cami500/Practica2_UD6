0d3c24d39a964763cfe4ec4d331a69b0116bb7f3
Nuria <nubogen@gmail.com>
2023-05-03 12:16:27 +0200

    [ADD] tarea 2

--- src/main/java/ed/nuria/Calculadora.java
+++ src/main/java/ed/nuria/Calculadora.java
@@ -1,19 +1,45 @@
 package ed.nuria;
 
+/**Esta clase hace las funciones básicas de una calculadora**/
+
+/** La clase calculadora realiza las operaciones de suma, resta,
+ * multiplicación y división  de números enteros**/
+
+/**
+ * @autor Nuria
+ * @version 3.7
+ * @since 2.1, junio de 2022**/
 public class Calculadora {
-	
+
+	/**Suma dos números enteros y devuelve el resultado
+	 * @param a primer sumando
+	 * @param b segundo sumando
+	 * @return la suma de a y b**/
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+
+	/**Resta dos números enteros y devuelve el resultado
+	 * @param a el minuendo
+	 * @param b el sustraendo
+	 * @return la resta de a y b**/
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	
+
+	/**Suma dos números enteros y devuelve el resultado
+	 * @param a primer factor
+	 * @param b segundo factor
+	 * @return la multipicacion de a y b**/
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
+
+	/**Suma dos números enteros y devuelve el resultado
+	 * @param a el dividendo
+	 * @param b el divisor
+	 * @return el cociente de a/b
+	 * @throws ArithmeticException si b es cero**/
 	public int diveix(int a, int b) {
 		return a/b;
 	}

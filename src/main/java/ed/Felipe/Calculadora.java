f10c566e7354619751890ebda3b617ac383c7fa9
Felizote <felizotefil@gmail.com>
2023-05-03 20:51:33 +0200

    Clase y funciones documentadas

--- src/main/java/ed/Felipe/Calculadora.java
+++ src/main/java/ed/Felipe/Calculadora.java
@@ -1,17 +1,16 @@
-package ed.karlos;
+package ed.Felipe;
 /**
- * Esta clase define una calculadora con cuatro métodos que realizan las operaciones básicas de suma, resta, multiplicación y división.
- *
+ * Esta clase define una calculadora
+ * Contiene cuatro métodos que realizan las operaciones básicas de suma, resta, multiplicación y división.
+ * @author Felipe Sánchez García
  * @version 3.7
  * @since 2.1
- * @author Karlos Andrés Chávez Abrego
  */
 
 public class Calculadora {
 	
 	/**
 	 * Devuelve la suma de dos números enteros.
-	 *
 	 * @param a El primer número entero.
 	 * @param b El segundo número entero.
 	 * @return El resultado de la suma de los dos números enteros.
@@ -23,7 +22,6 @@ public class Calculadora {
 	
 	/**
 	 * Devuelve la resta de dos números enteros.
-	 *
 	 * @param a El primer número entero.
 	 * @param b El segundo número entero.
 	 * @return El resultado de la resta de los dos números enteros.
@@ -34,7 +32,6 @@ public class Calculadora {
 	}
 	/**
 	 * Devuelve el resultado de la multiplicación de dos números enteros.
-	 *
 	 * @param a El primer número entero.
 	 * @param b El segundo número entero.
 	 * @return El resultado de los dos números enteros.
@@ -47,11 +44,10 @@ public class Calculadora {
 	
 	/**
 	 * Devuelve la división de dos números enteros.
-	 *
 	 * @param a El primer número entero.
 	 * @param b El segundo número entero (no puede ser cero).
-	 * @throws ArithmeticException Si el segundo número es cero.
 	 * @return El resultado.
+	 * @throws ArithmeticException Si el segundo número es cero.
        */
 	
 	public int diveix(int a, int b) {

0d4e421d028cf4bc8849b3b997ef73cf2a3742f1
Kokon44 <fracargar@alu.edu.gva.es>
2023-05-03 09:23:27 +0200

    Añadido la descripcion de cada metodo junto a cada parametro

--- src/main/java/ed/kico/Calculadora.java
+++ src/main/java/ed/kico/Calculadora.java
@@ -5,19 +5,24 @@ package ed.kico;
  *  con cuatro metodos definidos
  */
 public class Calculadora {
-	
+
+	//Metodo suma realiza la suma de dos numeros enteros y devuelve el resultado
+	//Parametros : a -> el primer numero que se desea sumar b -> el segundo numero que se desea sumar
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+	//Metodo resta realiza la resta de dos numeros enteros y devuelve el resultado
+	//Parametros : a → el primer numero que se desea restar b → el segundo número que se desea restar
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	
+	//Metodo multiplica realiza la multiplicación de dos numeros enteros y devuelve el resultado
+	//Parametros : a -> el primer numero que se desea multiplicar b -> el segundo numero que se desea multiplicar
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
+	//Metodo diveix realiza la division de dos numeros enteros y devuelve el resultado
+	//Parametros : a -> el primer numero que se desea dividir b -> el segundo numero que se desea dividir
 	public int diveix(int a, int b) {
 		return a/b;
 	}

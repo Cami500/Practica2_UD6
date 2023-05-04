eb42867991d37d1293f293594653e9d8ca4eac70
Alejandro Pons <aleponroc@alu.edu.gva.es>
2023-05-04 19:58:04 +0200

    [ADD] Apartado 6

--- src/main/java/ed/alejandro/Calculadora.java
+++ src/main/java/ed/alejandro/Calculadora.java
@@ -46,7 +46,7 @@ public class Calculadora {
 	 * @param b es el segundo factor
 	 * @return el resultado de la multiplicación de a por b
 	 */
-	public int multiplica(int a, int b){
+	public int multiplica(int a, int b) {
 		return a*b;
 	}
 	
@@ -61,5 +61,76 @@ public class Calculadora {
 	public int diveix(int a, int b) {
 		return a/b;
 	}
+	
+	/**
+	 * Este método realiza la operación de elevar un numero a su potencia n.
+	 *
+	 * @param base es el número a elevar
+	 * @param exponente es el exponente de la potencia
+	 * @return el resultado de elevar "a" a la enésima potencia
+	 */
+	public static int potencia(int base, int exponente) {
+	    int resultado = 1;
 
+	    for (int contador = 0; contador < exponente; contador++) {
+	        resultado = resultado * base;
+	    }
+
+	    return resultado; 
+	}
+	
+	/**
+	 * Este método comprueba si un número entero es par o impar.
+	 *
+	 * @param entero es el número entero a comprobar
+	 * @return true si el número es par, false en caso contrario
+	 */
+	public static boolean esPar(int entero) {
+	    boolean resultado;
+
+	    if (entero % 2 == 0) {
+	        resultado = true;
+	    } else {
+	        resultado = false;
+	    }
+
+	    return resultado;
+	}
+	
+	/**
+	 * Este método convierte un número decimal a su representación en base binaria.
+	 *
+	 * @param entero es el número decimal a convertir
+	 * @return una cadena de caracteres que representa en binario el número entero recibido como argumento
+	 */
+	public static String decimalABinario(int entero) {
+	    String representacionBinario = "";
+	    int cociente = entero;
+	    int resto;
+
+	    while (cociente > 0) {
+	        resto = cociente % 2;
+	        representacionBinario = resto + representacionBinario;
+	        cociente = cociente / 2;
+	    }
+	    
+	    return representacionBinario;
+	}
+	
+	/**
+	 * Método principal de la aplicación para realizar las correspondientes pruebas y depuración.
+	 * @param args son los argumentos recibidos por línea de comandos
+	 */
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		
+		int r1 = potencia(a, b);
+		boolean r2 = esPar(a);
+		String r3 = decimalABinario(b);
+		
+		System.out.printf("Resultado 1: %d, Resultado 2: %b, Resultado 3: %s", r1, r2, r3);
+		
+	}
+	
 }

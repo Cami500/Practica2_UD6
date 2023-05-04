683b92f16b9a086fc1deaf799f7ebfc14ce09a00
erikabp93 <eribalpas@alu.edu.gva.es>
2023-05-04 20:43:19 +0200

    Generado JavaDoc y solucionados los warnings que aparecían(borrats els altres comentaris)

--- src/main/java/ed/erika/Calculadora.java
+++ src/main/java/ed/erika/Calculadora.java
@@ -1,11 +1,4 @@
 package ed.erika;
-/**
- * Esta clase define una calculadora con cuatro métodos que realizan las operaciones básicas de suma, resta, multiplicación y división.
- *
- * @version 3.7
- * @since 2.1
- * @author Karlos Andrés Chávez Abrego
- */
 
 /**
  * En esta clase se define una calculadora en la que se pueden realizar operaciones matemáticas básicas como la suma, resta, multiplicación y división de dos números enteros
@@ -24,14 +17,6 @@ public class Calculadora {
 	}
 
 	/**
-	 * Devuelve la suma de dos números enteros.
-	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de la suma de los dos números enteros.
-	 */
-
-	/**
 	 * Devuelve la suma de los dos números enteros que se le pasa al método.
 	 * @param a Un número entero
 	 * @param b Otro número entero
@@ -41,14 +26,6 @@ public class Calculadora {
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
-	/**
-	 * Devuelve la resta de dos números enteros.
-	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de la resta de los dos números enteros.
-	 */
 
 	/**
 	 * Devuelve la resta de los dos números enteros que se le pasa al método.
@@ -61,13 +38,6 @@ public class Calculadora {
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	/**
-	 * Devuelve el resultado de la multiplicación de dos números enteros.
-	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de los dos números enteros.
-	 */
 
 	/**
 	 * Devuelve la multiplicación de los dos números enteros que se le pasa al método.
@@ -80,15 +50,6 @@ public class Calculadora {
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
-	/**
-	 * Devuelve la división de dos números enteros.
-	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero (no puede ser cero).
-	 * @throws ArithmeticException Si el segundo número es cero.
-	 * @return El resultado.
-       */
 
 	/**
 	 * Devuelve la división de los dos números enteros que se le pasa al método.

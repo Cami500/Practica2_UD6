83449de3cb8c8f954f41c4fcbe74b5b6f3246569
Jaume <jmg180802@gmail.com>
2023-05-04 10:56:10 +0200

    Segundo punto realizado

--- src/main/java/ed/jaume/Calculadora.java
+++ src/main/java/ed/jaume/Calculadora.java
@@ -4,59 +4,67 @@ package ed.jaume;
  *
  * @version 3.7
  * @since 2.1
- * @author Karlos Andrés Chávez Abrego
+ * @author Jaume Moltó Gallego
  */
 
 public class Calculadora {
-	
-	/**
-	 * Devuelve la suma de dos números enteros.
-	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de la suma de los dos números enteros.
-	 */
+
 	
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+
 	/**
-	 * Devuelve la resta de dos números enteros.
+	 * Devuelve la suma de dos números enteros.
 	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de la resta de los dos números enteros.
+	 * @param a Es el primer número entero.
+	 * @param b Es el segundo número entero.
+	 * @return El resultado de la suma de los dos números enteros.
 	 */
+	
+
 	
 	public int resta(int a, int b) {
 		return a - b;
 	}
+
 	/**
-	 * Devuelve el resultado de la multiplicación de dos números enteros.
+	 * Devuelve la resta de dos números enteros.
 	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero.
-	 * @return El resultado de los dos números enteros.
+	 * @param a Es el primer número entero.
+	 * @param b Es el segundo número entero.
+	 * @return El resultado de la resta de los dos números enteros.
 	 */
+
+
 	
 	
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
+
 	/**
-	 * Devuelve la división de dos números enteros.
+	 * Devuelve el resultado de la multiplicación de dos números enteros.
 	 *
-	 * @param a El primer número entero.
-	 * @param b El segundo número entero (no puede ser cero).
-	 * @throws ArithmeticException Si el segundo número es cero.
-	 * @return El resultado.
-       */
+	 * @param a Es el primer número entero.
+	 * @param b Ee el segundo número entero.
+	 * @return El resultado de los dos números enteros.
+	 */
+	
+
 	
 	public int diveix(int a, int b) {
 		return a/b;
 	}
+
+	/**
+	 * Devuelve el resultado de la división de dos números enteros.
+	 *
+	 * @param a Es el primer número entero.
+	 * @param b Es el segundo número entero tiene una excepción y es que no puede ser cero.
+	 * @throws ArithmeticException Si el segundo número es cero.
+	 * @return El resultado.
+	 */
 
 }
 

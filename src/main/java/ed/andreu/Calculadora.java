ecf412c47a41fdf71d67cd15ff77f581f4bcca80
Andreu Francés Gallego <andreufgromeral@gmail.com>
2023-05-24 09:57:29 +0200

    6. Agregar y comentar nuevo codigo

--- src/main/java/ed/andreu/Calculadora.java
+++ src/main/java/ed/andreu/Calculadora.java
@@ -60,5 +60,62 @@ public class Calculadora {
 	public int diveix(int a, int b) {
 		return a/b;
 	}
+
+    /**
+     * Hace la elevacion mediante la base y el exponente que se introducirá
+     * pasado a parametro.
+     * @param base es el número que se multiplicará en base al exponente
+     * @param exponente se indica las veces que multiplicaremos la base
+     * @return retorna la base introducida elevada al exponente introducido
+     * */
+    public int elevarNumero ( int base , int exponente ){
+
+        int resultado = 1;
+
+        for ( int i = 0; i < exponente ; i++ ){
+            resultado = resultado * base;
+        }
+
+        return resultado;
+    }
+
+    /**
+     * Este metodo hace la confirmación de sí el número pasado por
+     * parametro es par.
+     * @param numero es un valor que se pasa para confirmar si es par
+     * @return retorna un booleano confirmando si es un número par o no
+     * */
+    public boolean esNumeroPar ( int numero ){
+
+        boolean esNumeroPar;
+
+        if ( numero % 2 == 0) {
+            esNumeroPar = true;
+        } else {
+            esNumeroPar = false;
+        }
+
+        return esNumeroPar;
+    }
+
+    /**
+     * Este metodo hace la conversión de un número decimal pasado por parametro
+     * a binario.
+     * @param numeroDecimal es el valor que se transformara a binario
+     * @return el parametro transformado en binario
+     * */
+    public String convertirBinario(int numeroDecimal){
+
+        String resultado = "";
+        int numero = numeroDecimal;
+        int resto;
+
+        while ( numero > 0) {
+            resto = numero % 2;
+            resultado = resto + resultado;
+            numero = numero / 2;
+        }
+        return resultado;
+    }
 	
 }
